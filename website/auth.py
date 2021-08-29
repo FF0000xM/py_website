564fe80384f5a9c37db4c037aab9bedbcbb9a2d2
Michael <ff0000xm@gmail.com>
2021-08-30 00:05:27 +1000

    login/out/newcode

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,15 @@
 from  flask import Blueprint
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/in')
+def in():
+    return "<h2> Passcode? </h2>"
+
+@auth.route('/out')
+def out():
+    return "<h2> Bye! </h2>"
+
+@auth.route('/new-code')
+def new_code():
+    return "<h2> New Code. </h2>"
